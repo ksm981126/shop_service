cbb316ed34d9a9e7ecbf866475f06bb887aca707
ksm981126 <ksm981126@naver.com>
2022-02-04 11:56:45 +0900

    첫번째 프로젝트 업로드

---
+++ src/test/java/com/twogenesis/shoppingmall_service/ShoppingmallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twogenesis.shoppingmall_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
